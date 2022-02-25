8cd0dc222df27d64a53e106fa6cd9a136c28dd03
Aledeloss <Aledeloss@gmail.com>
2022-02-24 23:37:41 -0300

    Global styles, fonts and variables

---
+++ src/styles/variables.tsx
@@ -0,0 +1,42 @@
+import './importFont.css'
+
+// colors
+export const colors = {
+  superDarkBlue: '#052838',
+  cream: '#F4FFC2'
+}
+
+// fonts
+export const fonts = {
+  roboto: "'Roboto', sans-serif",
+  poppins: 'Poppins, san-serif'
+}
+
+// font-sizes
+export const fontSizes = {
+  font09: '9px',
+  font10: '10px',
+  font11: '11px',
+  font12: '12px',
+  font14: '14px',
+  font16: '16px',
+  font17: '17px',
+  font18: '18px',
+  font20: '20px',
+  font21: '21px',
+  font30: '30px',
+  font24: '24px'
+}
+
+// themes
+export const theme = {
+  colorPrimary: colors.cream,
+  colorSecondary: colors.superDarkBlue,
+  fontPrimary: fonts.poppins
+}
+
+export const breakpoints = {
+  desktop: '1024px',
+  tablet: '720px',
+  mobile: '520px'
+}
