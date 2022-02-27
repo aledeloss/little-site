fb8d3fa5b016a3a3292746bed20867f9efda8b8f
Aledeloss <Aledeloss@gmail.com>
2022-02-27 16:22:23 -0300

    Link styles

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -12,10 +12,16 @@ interface ILink {
 const StyledLink = styled.a`
   width: 30px;
   height: 30px;
+  
+  &:hover {
+    opacity: 0.6;
+    transform: scale(1.1);
+  }
 `
+
 const Link: React.FC<ILink> = ({ color, iconContent, url, title }: ILink) => {
   return (
-    <StyledLink href={url}>
+    <StyledLink href={url} target='_blank' data-testid={url}>
       <svg xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512">
         <title>{title}</title>
